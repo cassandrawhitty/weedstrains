b7846b12c9e0289673509555437aaca6b97fea92
JorPan <jbpanasewicz@gmail.com>
2020-12-04 12:28:10 -0700

    collecitng input values in an array

--- backendweeds/config/routes.rb
+++ backendweeds/config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :joiners
   resources :effects, only: [:index]
   resources :strains, only: [:index, :show]
+  get "filter", to: "strains#filter"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
